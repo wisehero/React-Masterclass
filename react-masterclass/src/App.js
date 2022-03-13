1250d4a3a7bed8fdd3e1279440e72e7a2a3a326c
김지웅 <godjw96@gmail.com>
2022-03-14 00:14:16 +0900

    Update App.js

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -2,6 +2,10 @@ import styled, { keyframes } from "styled-components";
 
 const Wrapper = styled.div`
   display: flex;
+  height: 100vh;
+  width: 100vw;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Btn = styled.button`
@@ -29,6 +33,10 @@ const rotationAnimation = keyframes`
   }
 `;
 
+const Emoji = styled.span`
+  font-size: 36px;
+`;
+
 const Box = styled.div`
   height: 200px;
   width: 200px;
@@ -37,14 +45,8 @@ const Box = styled.div`
   justify-content: center;
   align-items: center;
   animation: ${rotationAnimation} 1s linear infinite;
-  span {
-    font-size: 36px;
-    &:hover {
-      font-size: 48px;
-    }
-    &:active {
-      opacity: 0;
-    }
+  ${Emoji}:hover {
+    font-size: 98px;
   }
 `;
 
@@ -52,8 +54,9 @@ function App() {
   return (
     <Wrapper>
       <Box>
-        <span>❤️</span>
+        <Emoji>🔥</Emoji>
       </Box>
+      <Emoji>🔥</Emoji>
     </Wrapper>
   );
 }
