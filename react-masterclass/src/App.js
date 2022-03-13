88f37a0e61d10a3fb45474cf0493a5420af6389f
김지웅 <godjw96@gmail.com>
2022-03-13 23:50:31 +0900

    Animation and Pseudo Selector

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,6 +1,6 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
@@ -15,15 +15,46 @@ const Input = styled.input.attrs({ required: true, minlength: 10 })`
   background-color: tomato;
 `;
 
+const rotationAnimation = keyframes`
+  0% {
+    transform:rotate(0deg);
+    border-radius:0px;
+  }
+  50% {
+    border-radius:100px;
+  }
+  100%{
+    transform:rotate(360deg);
+    border-radius:0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 48px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
+
 function App() {
   return (
-    <Father>
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>❤️</span>
+      </Box>
+    </Wrapper>
   );
 }
 
