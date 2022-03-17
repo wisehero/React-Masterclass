372a7d0e05c0e5188a8948f51705b45e0459f804
김지웅 <godjw96@gmail.com>
2022-03-18 00:20:20 +0900

    Update Circle.tsx

--- typescript/src/Circle.tsx
+++ typescript/src/Circle.tsx
@@ -1,16 +1,16 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 interface ContainerProps {
-    bgColor:string;
-    borderColor: string;
-  }
-  
+  bgColor: string;
+  borderColor: string;
+}
+
 const Container = styled.div<ContainerProps>`
   width: 200px;
-  height:200px;
+  height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
-  border: 1px solid ${(props) => props.borderColor}
+  border: 1px solid ${(props) => props.borderColor};
 `;
 
 interface CircleProps {
@@ -19,14 +19,12 @@ interface CircleProps {
   text?: string;
 }
 
-function Circle({bgColor, borderColor, text = "default text"}: CircleProps) {
-    return (
-        <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
-            {text}
-        </Container>)
-        ;
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
 }
 
-
 export default Circle;
-
