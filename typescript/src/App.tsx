feb1ddfb77613f8c6c723fd94743c0123e834cc8
김지웅 <godjw96@gmail.com>
2022-03-20 02:57:47 +0900

    State

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,15 +1,12 @@
-import styled from "styled-components"
 import Circle from "./Circle";
-
 
 function App() {
   return (
     <div>
       <Circle bgColor="teal" borderColor="yellow"></Circle>
-      <Circle bgColor="tomato" text="im here"></Circle>
+      <Circle bgColor="tomato"></Circle>
     </div>
-  )
-  
+  );
 }
 
 export default App;
--- typescript/src/Circle.tsx
+++ typescript/src/Circle.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 interface ContainerProps {
@@ -16,14 +17,15 @@ const Container = styled.div<ContainerProps>`
 interface CircleProps {
   bgColor: string;
   borderColor?: string;
-  text?: string;
 }
 
-function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+function Circle({ bgColor, borderColor }: CircleProps) {
+  const [value, setValue] = useState<string>("");
   return (
-    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
-      {text}
-    </Container>
+    <Container
+      bgColor={bgColor}
+      borderColor={borderColor ?? bgColor}
+    ></Container>
   );
 }
 
