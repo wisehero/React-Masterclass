90f3a2ff299916664f3eefd5163651046cb40eba
김지웅 <godjw96@gmail.com>
2022-03-21 00:37:28 +0900

    #3.6 Themes

---
+++ typescript/src/theme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+export const lightTheme:DefaultTheme = {
+    bgColor: "whilte",
+    textColor: "black",
+    btnColor: "totato"
+}
+
+export const darkTheme:DefaultTheme = {
+    bgColor: "black",
+    textColor:"white",
+    btnColor: "totato"
+}
