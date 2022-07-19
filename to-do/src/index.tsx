7d55b020c1fd829a5fd5e54bce30214cb2b2cb25
wisehero10 <godjw96@gmail.com>
2022-07-19 11:16:21 +0900

    feat : project init

--- to-do/src/index.tsx
+++ to-do/src/index.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import {RecoilRoot} from "recoil";
 
 import App from "./App";
+import {ThemeProvider} from "styled-components";
+import {darkTheme} from "./theme";
 
 ReactDOM.render(
     <React.StrictMode>
         <RecoilRoot>
-            <App/>
+            <ThemeProvider theme={darkTheme}>
+                <App/>
+            </ThemeProvider>
         </RecoilRoot>
     </React.StrictMode>,
     document.getElementById("root")
